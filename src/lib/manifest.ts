f80d4642a30d3eb14e4f1ee0d9c09ebc591a3f2a
Tam Thai <hoangtam3062002@gmail.com>
2026-02-12 17:21:43 +0100

    add logo

--- src/lib/manifest.ts
+++ src/lib/manifest.ts
@@ -1,17 +1,19 @@
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 import { Manifest } from "stremio-addon-sdk";
 import pkg from "../../package.json" with { type: "json" };
+import { envGet } from "./env.js";
 
 const manifest: Manifest = {
   id: "community.yastream",
   version: pkg.version,
   catalogs: [],
   resources: ["stream"],
+  logo: `https://${envGet("DOMAIN")}/img/yas.png`,
   idPrefixes: ["tt", "tmdb:"],
   types: ["movie", "series"],
   name: "yastream",
   description:
-    "Stream asian movies, series and dramas with kisskh. Powered by TMDB for metadata",
+    "Yet Another Stream. Stream asian movies, series and dramas directly with kisskh. Powered by TMDB for metadata",
 };
 
 export default manifest;
